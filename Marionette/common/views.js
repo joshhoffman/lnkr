2699c178eda82238a4c3f4a24c5d14aff69ff806
joshhoffman <josh.edward.hoffman@gmail.com>
2014-08-13 22:51:37 -0500

    stupid loading view still not working

--- Marionette/common/views.js
+++ Marionette/common/views.js
@@ -6,7 +6,11 @@ module.exports = function(Views, LinkManager,
         template: loadingTemplate,
         title: "Loading Data",
         message: "test loading",
+        initialize: function() {
+            console.log('loading init');
+        },
         serializeData: function() {
+            console.log("loading serialize");
             return {
                 title: Marionette.getOption(this, "title"),
                 message: Marionette.getOption(this, "message")
